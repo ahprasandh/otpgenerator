00122f2b64376351408aa177b299f9c66dcb6d12
Hari <hariprasandh.a@zohocorp.com>
2021-09-17 12:42:50 +0530

    OTP Color changed on expiry

--- Readme.md
+++ Readme.md
@@ -9,16 +9,23 @@ npm install .
 Add config.json file in installed directory. Refer config.json.example
 
 
+## Global Installation
+To use totp command anywhere
+```
+npm install -g
+```
+
 ## Usage
 In terminal run
 ```
 totp
 ```
 
+## Authors
+[Hari Prasandh A](https://github.com/ahprasandh)
+
 ## Contributing
 Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
-
-
 
 ## License
 [MIT](https://choosealicense.com/licenses/mit/)
--- bin/app.js
+++ bin/app.js
@@ -7,7 +7,7 @@ var _ProgressBar = _interopRequireDefault(require("../bundle/ProgressBar"));
 var _totpGenerator = _interopRequireDefault(require("totp-generator"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-function printerror(msg){
+function printerror(msg) {
   console.log(chalk.red(msg));
 }
 const fs = require('fs');
@@ -15,7 +15,8 @@ const chalk = require('chalk');
 let codes;
 const Bar = new _ProgressBar.default();
 let sec = new Date().getSeconds();
-let runtime=0;
+let runtime = 0;
+let expireAlert = false;
 Bar.init(30);
 
 function sleep(ms) {
@@ -26,21 +27,23 @@ function sleep(ms) {
 
 function printTotp(name) {
   if (name && name.length > 0) {
+    let chalkColor = chalk.white;
+
+    chalkColor = (expireAlert && (sec % 2) == 0 ? chalk.red : chalk.white);
+
     !codes[name].period && (codes[name].period = 30);
-    console.log(chalk.cyan(name) + " " + (0, _totpGenerator.default)(codes[name].secret, {
+    console.log(chalk.cyan(name) + " " + (expireAlert && sec % 2 == 0 ? chalk.red : chalk.white)((0, _totpGenerator.default)(codes[name].secret, {
       period: codes[name].period
-    }) + "\n");
+    })) + `\n`);
   }
 };
 
 async function app() {
   console.clear();
-  console.log(chalk.black.bgCyan("*** TOTP GENERATOR @ahprasandh ***\n"));
-
+  console.log(chalk.black.bgCyan(`*** TOTP GENERATOR @ahprasandh ***\n`));
   for (var s in codes) {
     printTotp(s);
   }
-
   console.log("30s Timer:");
 }
 
@@ -52,30 +55,36 @@ async function main() {
   app();
   while (1) {
     sec++;
-    updateProgress();
+    runtime++;
     if (sec > 30) {
+      expireAlert = false;
       sec -= 30;
-      runtime+=30;
       app();
-      updateProgress();
+    } else if (sec > 25 || runtime > 50) {
+      expireAlert = true;
+      app();
+    } else {
+      expireAlert = false;
     }
-    if(runtime > 60){
-      console.log();
+    if (runtime > 60) {
+      console.clear();
+      console.log(`*** TOTP GENERATOR @ahprasandh ***`);
       return;
     }
+    updateProgress();
     await sleep(1000);
   }
 }
 
-fs.readFile(__dirname+'/../config.json', 'utf8', function (err,data) {
+fs.readFile(__dirname + '/../config.json', 'utf8', function (err, data) {
   if (err) {
     console.log(err)
     printerror("Config.json not proper");
     return;
   }
-  try{
-    codes=JSON.parse(data)
-  }catch(e){
+  try {
+    codes = JSON.parse(data)
+  } catch (e) {
     printerror("Config.json not proper");
     return;
   }
